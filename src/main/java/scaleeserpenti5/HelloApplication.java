5a615a043412b95dba30e4fdc7977fab76eb8b6f
Francesco <francescofratto2002@gmail.com>
2024-09-08 16:16:37 +0200

    Migrazione JavaFX

---
+++ src/main/java/scaleeserpenti5/HelloApplication.java
@@ -0,0 +1,17 @@
+package scaleeserpenti5;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
